4fdd24894aa984307d1e6b955522815cb159dcb6
Manuel Soares <manuel.r.soares@tecnico.ulisboa.pt>
2024-10-12 23:37:27 +0100

    Submissão 2

--- FP2425P1.py
+++ FP2425P1.py
@@ -70,50 +70,57 @@ def obtem_diagonais(tab, pos):
     tamanho_linha = len(tab[0])
     tamanho_coluna = len(tab)
     max = tamanho_coluna * tamanho_linha
+
     iniciais_diagonais = (tuple(range(1, tamanho_linha +1)))
     for i in range(1, tamanho_coluna):
         iniciais_diagonais += (i * tamanho_linha + 1,)
-    print(iniciais_diagonais)
 
     iniciais_antidiagonais = tuple(range(max, max - tamanho_linha, -1))
     for i in range(0,tamanho_coluna - 1):
         iniciais_antidiagonais += (i * tamanho_linha + 1,)
-    print(iniciais_antidiagonais)
+
     tuplo_diagonais = ()
     tuplo_antidiagonais = ()
     for inicio in iniciais_diagonais:
         if pos in tuple(range(inicio, max, tamanho_linha + 1)):
             tuplo_diagonais = tuple(range(inicio, max, tamanho_linha + 1))
+
     for inicio in iniciais_antidiagonais:
         if pos in tuple(range(inicio, 0, - tamanho_linha + 1)):
             tuplo_antidiagonais = tuple(range(inicio, 0, - tamanho_linha + 1))
     return (tuplo_diagonais, tuplo_antidiagonais)
-        
-#rint(obtem_diagonal(((1,0,0,1),(-1,1,0,1), (-1,0,0,-1)), 6))
 
+def tabuleiro_para_str(tab): 
+    representacao_tab = ''
 
-
-
-def tabuleiro_str(tab): 
-
-    representacao = ''
-
-    for m in tab:
-        representacao_linha = '' #'X---+---+\n| | |\nO---X---+\n| | |\nO---+---+'
-        for n in m:
-            if n == 1:
-                n = 'O'
-            elif n == -1:
-                n = 'X'
+    for i_linha in range(len(tab)):
+        linha = tab[i_linha]
+        for i_col in range(len(linha)):
+            col = linha[i_col]
+            if col == 1:
+                representacao_tab += 'X'
+            elif col == -1:
+                representacao_tab += 'O'
             else:
-                n = '+'
-    representacao += representacao_linha + '\n'
-        
-    return representacao
+                representacao_tab += '+'
+            
+            if i_col < len(linha) - 1:
+                representacao_tab += '---'
+    
+        if i_linha < len(tab) - 1:
+            representacao_tab += '\n'
+            for col in range(len(tab[0])):
+                representacao_tab += '|'
+                if col < len(linha) - 1:
+                    representacao_tab += '   '
+            representacao_tab += '\n'
+
+    return representacao_tab
 
 def eh_posicao_valida(tab, pos): 
-    if isinstance(tab, tuple) and isinstance(pos, int):
-        if 1 <= pos <= 100 * 100:
+
+    if eh_tabuleiro(tab) and isinstance(pos, int):
+        if 1 <= pos <= len(tab) * len(tab[0]):
             return True
     else:
         raise ValueError('eh_posicao_valida: argumentos invalidos')
@@ -124,27 +131,116 @@ def eh_posicao_livre(tab, pos):
         if pos < 0 or pos >= len(tab[0]):
            return False
         
-    for m in tab:
-        if m[pos] == 0:
+    for linha in tab:
+        if linha[pos] == 0:
             return True
     else:
         raise ValueError('eh_posicao_livre: argumentos invalidos')
     
-def obtem_posicoes_livres(tab): # A função não está corretamente, tenho que voltar a fazer...
-    posicoes_livres = ()
-    for m in tab:
-        for i in range(len(m)):
-            if  m[i] == 0:
-                posicoes_livres += (i + 1,)
-    return posicoes_livres
+def obtem_posicoes_livres(tab): 
+    if not eh_tabuleiro(tab):
+        raise ValueError('obtem_posicoes_livres: argumento invalido')
+    else:
+        posicoes_livres = ()
 
-def obtem_posicoes_jogadores(tab, int):
-    posicoes_jogadores = ()
-    if int != 1 or int != -1 or int != 0:
-        raise ValueError('obtem_posicoes_jogadores: argumentos invalidos')
-    for m in tab:
-        for i in range(len(m)):
-            if m[i] == int:
-                posicoes_jogadores += (i + 1,)
-    posicoes_jogadores_ord = list(sorted(posicoes_jogadores))
-    return tuple(posicoes_jogadores_ord)
+        for linha in range(len(tab)):
+            for col in range(len(tab[linha])):
+                if  tab[linha][col] == 0:
+                    posicoes_livres += (linha * len(tab[0]) + col + 1,)
+        return posicoes_livres
+
+def obtem_posicoes_jogador(tab, jog):
+    if not eh_tabuleiro(tab) or not (jog in [-1, 0, 1]):
+        raise ValueError('obtem_posicoes_jogador: argumentos invalidos')
+    else: # É preciso?
+        tuplo_posicoes = ()  
+        for linha in range(len(tab)): # 
+            for col in range(len(tab[linha])):
+                if  tab[linha][col] == jog:
+                    tuplo_posicoes += (linha * len(tab[0]) + col + 1,)
+        return tuplo_posicoes
+    
+def obtem_posicoes_adjacentes(tab, pos):
+    dimensoes = obtem_dimensao(tab)
+    max_posicao = dimensoes[0] * dimensoes[1]
+    if not (eh_tabuleiro(tab) and isinstance(pos, int) and (1 <= pos <= max_posicao)):
+        raise ValueError('obtem_posicoes_adjacentes: argumentos invalidos')
+    else:
+        tuplo_adjacentes = ()
+        tamanho_linha = len(tab[0])
+
+        index_linha_pos = (pos -1) // tamanho_linha
+        index_coluna_pos = (pos -1) % tamanho_linha
+
+        for index_linha in range(index_linha_pos - 1, index_linha_pos + 2):
+            for index_coluna in range(index_coluna_pos - 1, index_coluna_pos + 2):
+                posicao_adjacente = index_linha * tamanho_linha + index_coluna + 1
+
+                condicao_linha_esperada = index_linha == ((posicao_adjacente - 1) // tamanho_linha)
+                condicao_pertencer_tabuleiro = 1 <= posicao_adjacente <= max_posicao
+
+                if condicao_linha_esperada and condicao_pertencer_tabuleiro and posicao_adjacente != pos:
+                    tuplo_adjacentes += (posicao_adjacente,)
+
+        return tuplo_adjacentes
+    
+
+def ordena_posicoes_tabuleiro(tab, tup):
+    if not (eh_tabuleiro(tab) and isinstance(tup, tuple)):
+        raise ValueError('ordena_posicoes_tabuleiro: argumentos invalidos')
+    else:
+        dimensao = obtem_dimensao(tab)
+        m = dimensao[0]
+        n = dimensao[1]
+
+        posicao_central = (m // 2) * n + (n // 2) + 1
+        
+        def distancia(pos1, pos2):
+            
+            linha1 = (pos1 - 1) // n
+            coluna1 = (pos1 - 1) % n
+
+            linha2 = (pos2 - 1) // n
+            coluna2 = (pos2 - 1) % n
+
+            return max(abs(linha2 - linha1), abs(coluna2 - coluna1))
+        
+        tuplo_distancias = ()
+        for elemento in tup:
+            distancia_central = distancia(posicao_central, elemento)
+            tuplo_elemento_distancia = (elemento, distancia_central)
+            tuplo_distancias += (tuplo_elemento_distancia,)
+
+        tuplo_distancias = sorted(tuplo_distancias, key = lambda x: (x[1], x[0])) # 1 é a distancia e 0 é a posição
+
+        tuplo_final = ()
+        for elemento in tuplo_distancias:
+            tuplo_final += (elemento[0],)
+
+    return tuplo_final
+
+def marca_posicao(tab, pos, jog):
+    condicao_validacao_tab = eh_tabuleiro(tab) and eh_posicao_valida(tab, pos) and (jog in (-1, 1))
+    if not condicao_validacao_tab:
+        raise ValueError('marca_posicao: argumentos invalidos')
+
+    else:
+        dimensoes = obtem_dimensao(tab)
+        tamanho_linha = dimensoes[1]
+
+        index_linha_pos = (pos - 1) // tamanho_linha
+        index_coluna_pos = (pos - 1) % tamanho_linha
+
+        tabuleiro_marcado = ()
+        for index_linha in range(len(tab)):
+            tuplo_aux = ()
+            for index_coluna in range(len(tab[index_linha])):
+                if index_coluna == index_coluna_pos and index_linha == index_linha_pos:
+                    tuplo_aux += (jog,)
+                    continue
+                tuplo_aux += (tab[index_linha][index_coluna],)
+            tabuleiro_marcado += (tuplo_aux,)
+
+    return tabuleiro_marcado
+
+
